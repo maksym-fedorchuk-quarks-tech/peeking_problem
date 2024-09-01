2444d7eb994e275655b4d85c71ced4b2646cd03d
mfedorchuk <maksym.fedorchuk@corp.kismia.com>
2024-09-01 15:04:07 +0300

    master: initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # peeking_problem
-Peeking Problem experiment
+
+Here we going to do experiment for the Peeking Problem explanation
+
+A brief definition of the Peeking Problem is that it arises when there is 
+constant "peeking" at the test results before they reach statistical 
+significance or the planned and predetermined duration of the test. As a result,
+this can lead to incorrect data interpretation.
+
+Most of analytics do split tests (or are present during the process - it's how 
+lucky they are), and generally, they know that a standard test (like the 
+Student's t-test) doesn't answer the question, "Is the new payment screen better?" 
+but rather answers, "What is the probability that the differences between our 
+test and control groups are due to chance?". And if an analyst has agreed with a
+manager that the test will be accepted at a 95% significance level (meaning that
+they choose a 5% probability that the result is not random), the practice of 
+peeking allows for the "accidental" acceptance of a test (which shouldn't have 
+been accepted) with a much higher probability 🙂. The materials I've come across 
+online suggest that, based on their experiments, this percentage can reach up
+to 20%. And this potentially means that every 5th rejected split-test rejected 
+with no good reason 😣...
---
+++ synthetic_data_first_test.py
@@ -0,0 +1,76 @@
+import numpy as np
+from scipy import stats
+
+# dataset params
+SIMULATIONS_NUM = 1000
+SAMPLE_SIZE = 500000
+AVG_BUYERS_PERCENTAGE = 0.02
+
+# t-Test param (Minimum p-value we need to "accept the test").
+ALPHA = 0.05
+# Every 'PEEKING_STEP_SIZE' observations we going to check the p-value
+PEEKING_STEP_SIZE = 50000
+
+
+def generate_synthetic_data(user_num: int, buyers_percentage: float):
+    """Generate two datasets representing user simulations. Each element in the
+    arrays indicates whether a user is a buyer (1) or not (0).
+
+    Parameters:
+        user_num (int): Number of users (simulations) in each dataset.
+        buyers_percentage (float): Probability that a user is a buyer (1).
+
+    Returns:
+        tuple: Two numpy arrays of size N."""
+
+    group1 = np.random.choice([0, 1], size=user_num, p=[1 - buyers_percentage, buyers_percentage])
+    group2 = np.random.choice([0, 1], size=user_num, p=[1 - buyers_percentage, buyers_percentage])
+
+    return group1, group2
+
+
+def analyze_without_peeking(group1: np.ndarray, group2: np.ndarray) -> int:
+    """Return p-value for whole test data samples"""
+    t_stat, p_value = stats.ttest_ind(group1, group2)
+
+    return p_value
+
+
+def analyze_with_peeking(group1: np.ndarray, group2: np.ndarray, step_size) -> list:
+    """Return list of all observed p-values, representing the results of peeking
+    at the data after every 'step_size' number of samples."""
+    p_values = []
+    for i in range(step_size, len(group1) + 1, step_size):
+        t_stat, p_value = stats.ttest_ind(group1[:i], group2[:i])
+        p_values.append(p_value)
+
+    return p_values
+
+
+if __name__ == "__main__":
+    peeking_significant_results = 0
+    non_peeking_significant_results = 0
+
+    for i in range(SIMULATIONS_NUM):
+        control_group, test_group = generate_synthetic_data(SAMPLE_SIZE, AVG_BUYERS_PERCENTAGE)
+        p_value_non_peeking = analyze_without_peeking(control_group, test_group)
+        p_values_peeking = analyze_with_peeking(control_group, test_group, PEEKING_STEP_SIZE)
+
+        if p_value_non_peeking < ALPHA:
+            non_peeking_significant_results += 1
+
+        if any(p < ALPHA for p in p_values_peeking):
+            peeking_significant_results += 1
+
+        print(f"Iteration {i} of {SIMULATIONS_NUM}") if i % 1000 == 0 else None
+
+    print(
+        "-" * 80,
+        f"simulations number: {SIMULATIONS_NUM}",
+        f"sample_size: {SAMPLE_SIZE}",
+        f"alpha: {ALPHA}",
+        f"peeking every N observations: {PEEKING_STEP_SIZE}",
+        f"non_peeking_significant_results: {non_peeking_significant_results}",
+        f"peeking_significant_results: {peeking_significant_results}",
+        sep="\n",
+    )
